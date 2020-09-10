6c2a461e444d0ae9160099a7b45e13048c97612e
Andrey Ivanov <andrey.ivanov@okko.tv>
2020-09-10 12:47:34 +0600

    remove sw

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vk-connect';
 import App from './App';
-// import registerServiceWorker from './sw';
 
 // Init VK  Mini App
-connect.send('VKWebAppInit');
-
-// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
-// расскомментируйте строку с registerServiceWorker();
-// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
-// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
-// registerServiceWorker();
+// connect.send('VKWebAppInit');
 
 ReactDOM.render(<App />, document.getElementById('root'));
